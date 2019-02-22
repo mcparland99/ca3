e5cf0e43198283be8daced43b68fc6ba01c2096a
caolam <caola@LAPTOP-BSV5TGPL.home>
2019-02-22 16:26:51 +0000

    Commit #3. Added a navigation bar to the header.

--- view/header.php
+++ view/header.php
@@ -1,10 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Football League Information</title>
+    <title>Football League Fixtures</title>
     <link href="../main.css" rel="stylesheet" type="text/css"/>
 </head>
 
 <body>
-<header><h1>Football League Information</h1></header>
+<header><h1>Football League Fixtures</h1>
+<div class="topnav">
+    <a href="fixtures_manager/index.php">Fixtures Manager</a>
+    <a href="team_info/index.php">Team Information</a>
+</header>
+
        
