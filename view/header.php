0715c8b36eb543859defdadc23257eae0c339d7e
caolam <caola@LAPTOP-BSV5TGPL.home>
2019-03-01 18:58:19 +0000

    Commit #13. Added file path for the favicon.ico file.

--- view/header.php
+++ view/header.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Football League Fixtures</title>
-    <link href="../main.css" rel="stylesheet" type="text/css"/>
-</head>
+    <head>
+        <title>Football League Fixtures</title>
+        <link href="../main.css" rel="stylesheet" type="text/css"/>
+        <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+        <link rel="shortcut icon" href="../images/favicon.ico" type="image/x-icon">
 
-<body>
-<header><h1>Football League Fixtures</h1>
-<div class="topnav">
-    <a href="fixtures_manager/index.php">Fixtures Manager</a>
-    <a href="team_info/index.php">Team Information</a>
-</header>
+    </head>
 
-       
+    <body>
+        <header><h1>Football League Fixtures</h1>
+            <div class="topnav">
+                <a href="fixtures_manager/index.php">Fixtures Manager</a>
+                <a href="team_info/index.php">Team Information</a>
+        </header>
+
