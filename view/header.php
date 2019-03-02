98f953ba28b9156d53a0aaa2ef9dfcff6b74dc49
caolam <caola@LAPTOP-BSV5TGPL.home>
2019-03-02 13:09:55 +0000

    Commit #19. Updated the header.

--- view/header.php
+++ view/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Football League Fixtures</title>
+        <title>Football League Standings</title>
         <link href="../main.css" rel="stylesheet" type="text/css"/>
         <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
         <link rel="shortcut icon" href="../images/favicon.ico" type="image/x-icon">
@@ -9,7 +9,7 @@
     </head>
 
     <body>
-        <header><h1>Football League Fixtures</h1>
+        <header><h1>Football League Standings</h1>
             <div class="topnav">
                 <a href="fixtures_manager/index.php">Fixtures Manager</a>
                 <a href="team_info/index.php">Team Information</a>
