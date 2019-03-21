a1baa619ac894bbc6ed30090668493048a620b34
Eoin Reid <eoinreid@protonmail.com>
2019-03-21 09:53:35 +0000

    side-bar navigation
    Adds a side-bar style nav bar (bare) to allow for easy navigation 
    throughout the application with href links to fixtures_controller and
    team_controller.

--- view/header.php
+++ view/header.php
@@ -2,17 +2,71 @@
 <html>
     <head>
         <title>Football League Standings</title>
+        <!-- Main CSS -->
         <link href="../css/main.css" rel="stylesheet" type="text/css"/>
-        <link href="../css/simple-sidebar.css" rel="stylesheet" type="text/css"/>
+        <!-- Bootstrap core CSS -->
+        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Sidebar CSS -->
+        <link href="css/simple-sidebar.css" rel="stylesheet">
+
+        <!-- Favicon  -->
         <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
-        <link rel="shortcut icon" href="../images/favicon.ico" type="image/x-icon">
+
 
     </head>
 
     <body>
-        <header><h1>Football League Standings</h1>
-            <div class="topnav">
-                <a href="fixtures_contorller/index.php">Fixtures Manager</a>
-                <a href="team_info/index.php">Team Information</a>
-        </header>
+
+        <div class="d-flex" id="wrapper">
+
+            <!-- Sidebar -->
+            <div class="bg-light border-right" id="sidebar-wrapper">
+                <div class="sidebar-heading">Football League Standings</div>
+                <div class="list-group list-group-flush">
+                    <a href="index.php" class="list-group-item list-group-item-action bg-light">Dashboard</a>
+                    <a href="fixtures_controller/" class="list-group-item list-group-item-action bg-light">Fixtures Manager</a>
+                    <a href="team_controller" class="list-group-item list-group-item-action bg-light">Team Information</a>
+                    <a href="#" class="list-group-item list-group-item-action bg-light">About Application</a>
+                </div>
+            </div>
+            <!-- /#sidebar-wrapper -->
+
+
+
+            <!-- Page Content -->
+            <div id="page-content-wrapper">
+
+                <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+                    <button class="btn btn-primary" id="menu-toggle">Toggle Menu</button>
+
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                  
+                </nav>
+
+                <div class="container-fluid">
+                    <h1 class="mt-4">Football League Standings App</h1>
+                    <p>The starting state of the menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will change.</p>
+                    <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>. The top navbar is optional, and just for demonstration. Just create an element with the <code>#menu-toggle</code> ID which will toggle the menu when clicked.</p>
+                </div>
+            </div>
+            <!-- /#page-content-wrapper -->
+
+        </div>
+        <!-- /#wrapper -->
+
+        <!-- Bootstrap core JavaScript -->
+        <script src="vendor/jquery/jquery.min.js"></script>
+        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+        <!-- Menu Toggle Script -->
+        <script>
+            $("#menu-toggle").click(function (e) {
+                e.preventDefault();
+                $("#wrapper").toggleClass("toggled");
+            });
+        </script>
 
