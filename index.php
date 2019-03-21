4d583a43b0502bcaf051ee647992f5bf7e0f8431
Eoin Reid <eoinreid@protonmail.com>
2019-03-21 09:54:34 +0000

    index.php side-bar navigation
    Edits index.php to allow for the new side-bar navigation to replace the
    old style nav bar and removes all information from index.php to the 
    header include.

--- index.php
+++ index.php
@@ -1,15 +1,4 @@
 <?php include 'view/header.php'; ?>
-<link href="css/main.css" rel="stylesheet" type="text/css"/>
-<link href="css/simple-sidebar.css" rel="stylesheet" type="text/css"/>
-<main>
-    <h1>Menu</h1>
-    <ul>
-        <li>
-            <a href="fixtures_controller">Standings Manager</a>
-        </li>
-        <li>
-            <a href="team_info">Team Information</a>
-        </li>
-    </ul>
-</main>
+
+
 <?php include 'view/footer.php'; ?>
