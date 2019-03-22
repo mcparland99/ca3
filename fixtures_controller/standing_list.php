f75709149dd878300cf1dad007c337910ff9f108
Eoin Reid <eoinreid@protonmail.com>
2019-03-22 16:13:30 +0000

    sidebar nav link bugfix
    Fixes a bug where the sidebar nav link would throw an error due to
    going to the wrong index page i.e while on /fixtures_controller/ 
    clicking dashboard should bring you to /index.php/ but instead would
    go to /fixtures_controller/index.php. To fix this added a ../ before
    the link to go back to the root.

--- fixtures_controller/standing_list.php
+++ fixtures_controller/standing_list.php
@@ -18,8 +18,17 @@
 
     <div class="d-flex" id="wrapper">
 
-        <?php include '../view/sidebar_nav.php'; ?>
-
+  <!-- Sidebar -->
+            <div class="bg-light border-right" id="sidebar-wrapper">
+                <div class="sidebar-heading">Football League Standings</div>
+                <div class="list-group list-group-flush">
+                    <a href="../index.php" class="list-group-item list-group-item-action bg-light">Dashboard</a>
+                    <a href="fixtures_controller/" class="list-group-item list-group-item-action bg-light">Fixtures Manager</a>
+                    <a href="../team_controller/" class="list-group-item list-group-item-action bg-light">Team Information</a>
+                    <a href="#" class="list-group-item list-group-item-action bg-light">About Application</a>
+                </div>
+            </div>
+            <!-- /#sidebar-wrapper -->
 
         <!-- Page Content -->
         <div id="page-content-wrapper">
