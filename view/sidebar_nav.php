d01e7078175c263e6ad72dc5d198b90a559493d8
Eoin Reid <eoinreid@protonmail.com>
2019-03-22 14:37:23 +0000

    sidebar_nav view
    Adds the sidebar nav to the sidebar_nav.php in the view folder to allow
    for ease of use in a php include function as part of the MVC structure.

---
+++ view/sidebar_nav.php
@@ -0,0 +1,11 @@
+ <!-- Sidebar -->
+            <div class="bg-light border-right" id="sidebar-wrapper">
+                <div class="sidebar-heading">Football League Standings</div>
+                <div class="list-group list-group-flush">
+                    <a href="index.php" class="list-group-item list-group-item-action bg-light">Dashboard</a>
+                    <a href="fixtures_controller/" class="list-group-item list-group-item-action bg-light">Fixtures Manager</a>
+                    <a href="team_controller" class="list-group-item list-group-item-action bg-light">Team Information</a>
+                    <a href="#" class="list-group-item list-group-item-action bg-light">About Application</a>
+                </div>
+            </div>
+            <!-- /#sidebar-wrapper -->
