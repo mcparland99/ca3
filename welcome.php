7858ee8e9651c50b8117a1bbbde6e1dc5331067a
d00215894 <d00215894@P1105-PC08.ad.dkit.ie>
2019-03-25 15:10:24 +0000

    Welcome page created. User is automatically directed here after registering or logging in.

---
+++ welcome.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+ 
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+        <link href="main.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+    <div class="page-header">
+        <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+    </div>
+    <p>
+        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+        <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
+    </p>
+</body>
+</html>
