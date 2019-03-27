5b960d895249b8813a03ca80d5699fb2de080fdc
d00215894 <d00215894@P1105-PC08.ad.dkit.ie>
2019-03-27 23:30:27 +0000

    Updated the add and edit functions.

--- model/player_db.php
+++ model/player_db.php
@@ -45,31 +45,34 @@ function delete_player($player_id) {
     $statement->closeCursor();
 }
 
-function add_player($player_name, $league_id, $goals_scored) {
+function add_player($league_id, $name, $team, $goals_scored) {
     global $db;
     $query = 'INSERT INTO players
-                 (playerName, leagueID, goalsScored)
+                 (leagueID, name, team, goalsScored)
               VALUES
-                 (:player_name, :league_id, :goals_scored)';
+                 (:league_id, :name, :team, :goals_scored)';
     $statement = $db->prepare($query);
-    $statement->bindValue(':player_name', $player_name);
     $statement->bindValue(':league_id', $league_id);
+    $statement->bindValue(':name', $name);
+    $statement->bindValue(':team', $team);
     $statement->bindValue(':goals_scored', $goals_scored);
     $statement->execute();
     $statement->closeCursor();
 }
 
-function update_player($player_id ,$player_name, $league_id, $goals_scored) {
+function update_player($player_id, $league_id, $name, $team, $goals_scored) {
     global $db;
     $query = 'UPDATE players
-              SET playerName = :player_name,
-                  leagueID = :league_id,
+              SET leagueID = :league_id,
+                  name = :name,
+                  team = :team,
                   goalsScored = :goals_scored,
                WHERE playerID = :player_id';
     $statement = $db->prepare($query);
-    $statement->bindValue(':player_name', $player_name);
     $statement->bindValue(':league_id', $league_id);
-    $statement->bindValue(':goals_scored', $goals_scored);;
+    $statement->bindValue(':name', $name);
+    $statement->bindValue('team', $team);
+    $statement->bindValue(':goals_scored', $goals_scored);
     $statement->bindValue(':player_id', $player_id);
     $statement->execute();
     $statement->closeCursor();
