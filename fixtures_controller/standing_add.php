911e4cd8030f3f3833e8de0a3c3ece34dc338e5e
Eoin Reid <eoinreid@protonmail.com>
2019-03-29 14:13:35 +0000

    standing_add form sidebar & bootstrap css styling
    Adds the sidebar navagation to the standings add form aswell as the
    bootstrap css style to allow the form to match the rest of the 
    sites styling and color scheme.

--- fixtures_controller/standing_add.php
+++ fixtures_controller/standing_add.php
@@ -1,38 +1,84 @@
-<?php include '../view/header.php'; ?>
-<main>
-    <h1>Add Standing</h1>
-    <form action="index.php" method="post" id="add_standing_form">
-        <input type="hidden" name="action" value="add_standing">
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Football League Standings</title>
 
-        <label>League</label>
-        <select name="league_id">
-        <?php foreach ( $leagues as $league ) : ?>
-            <option value="<?php echo $league['leagueID']; ?>">
-                <?php echo $league['leagueName']; ?>
-            </option>
-        <?php endforeach; ?>
-        </select>
-        <br>
+        <!-- Bootstrap core CSS -->
+        <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-        <label>Team:</label>
-        <input type="input" name="team">
-        <br>
+        <!-- Sidebar CSS -->
+        <link href="../css/simple-sidebar.css" rel="stylesheet">
 
-        <label>Wins:</label>
-        <input type="input" name="wins">
-        <br>
+        <!-- Favicon  -->
+        <link rel="shortcut icon" href="../images/favicon.ico" type="image/x-icon">
 
-        <label>Points:</label>
-        <input type="input" name="points">
-        <br>
 
-        <label>&nbsp;</label>
-        <input type="submit" value="Add Standing">
-        <br>
-    </form>
-    <p class="last_paragraph">
-        <a href="index.php?action=list_standings">View Standings</a>
-    </p>
+    </head>
+    <body>
+        <div class="d-flex" id="wrapper">
 
-</main>
-<?php include '../view/footer.php'; ?>
+            <!-- Sidebar -->
+            <div class="bg-light border-right" id="sidebar-wrapper">
+                <div class="sidebar-heading">Football League Standings</div>
+                <div class="list-group list-group-flush">
+                    <a href="../index.php" class="list-group-item list-group-item-action bg-light">Dashboard</a>
+                    <a href="fixtures_controller/" class="list-group-item list-group-item-action bg-light">Fixtures Manager</a>
+                    <a href="../team_controller/" class="list-group-item list-group-item-action bg-light">Team Information</a>                 
+                </div>
+            </div>
+            <!-- /#sidebar-wrapper -->
+            <div id="page-content-wrapper">
+
+
+                <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+                    <button class="btn btn-primary" id="menu-toggle">Toggle Menu</button>
+
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+
+                </nav>
+
+                <div class="container-fluid">
+                    <h1>Add Standing</h1>
+                    <form action="index.php" method="post" id="add_standing_form">
+                        <input type="hidden" name="action" value="add_standing">
+
+                        <label>League</label>
+                        <select name="league_id">
+                            <?php foreach ($leagues as $league) : ?>
+                                <option value="<?php echo $league['leagueID']; ?>">
+                                    <?php echo $league['leagueName']; ?>
+                                </option>
+                            <?php endforeach; ?>
+                        </select>
+                        <br>
+
+                        <label>Team:</label>
+                        <input type="input" name="team">
+                        <br>
+
+                        <label>Wins:</label>
+                        <input type="input" name="wins">
+                        <br>
+
+                        <label>Points:</label>
+                        <input type="input" name="points">
+                        <br>
+
+                        <label>&nbsp;</label>
+                        <input type="submit" value="Add Standing">
+                        <br>
+                    </form>
+                    <p class="last_paragraph">
+                        <a href="index.php?action=list_standings">View Standings</a>
+                    </p>
+
+                </div>
+            </div>
+
+        </div>
+
+    </body>
+</html>
