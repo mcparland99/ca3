c5c5b90acb2a491c371282d36691f748476ebf1f
Eoin Reid <eoinreid@protonmail.com>
2019-04-28 21:36:34 +0100

    Player index page styling
    Adds the bootstrap styling for the player index page.

---
+++ players_controller/player_list.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Football League Standings</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Sidebar CSS -->
+        <link href="../css/simple-sidebar.css" rel="stylesheet">
+
+        <!-- Favicon  -->
+        <link rel="shortcut icon" href="../images/favicon.ico" type="image/x-icon">
+
+
+    </head>
+    <body>
+        <div class="d-flex" id="wrapper">
+            
+                   <!-- Sidebar -->
+            <div class="bg-light border-right" id="sidebar-wrapper">
+                <div class="sidebar-heading">Football League Standings</div>
+                <div class="list-group list-group-flush">
+                    <a href="../index.php" class="list-group-item list-group-item-action bg-light">Dashboard</a>
+                    <a href="../standings_controller" class="list-group-item list-group-item-action bg-light">Fixtures Manager</a>
+                    <a href="../team_controller/" class="list-group-item list-group-item-action bg-light">Team Information</a>
+                    <a href="players_controller" class="list-group-item list-group-item-action bg-light">Player Information</a>
+                </div>
+                <div class="list-group-item-light" id="sidebar-wrapped">
+                    <div>
+                    <a href="../logout.php" >Log out</a>
+                    </div>
+                    <div>
+                    <a href="../reset-password.php" >Reset password</a>
+                    </div>
+                   
+                </div>
+                
+            </div>
+      
+            <!-- /#sidebar-wrapper -->
+              
+            <div id="page-content-wrapper">
+                
+                 <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+                    <button class="btn btn-primary" id="menu-toggle">Toggle Menu</button>
+
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </nav>        
+
+                <div class="container-fluid">
+                     <h1>Players</h1>
+
+  
+        <h2>Leagues</h2>
+        <?php include '../view/league_nav.php'; ?>        
+ 
+        <h2><?php echo $league_name; ?></h2>
+        <table>
+            <tr>
+                <th>Name:</th>
+                <th>Team:</th>
+                <th class="right">Goals Scored:</th>
+                <th>Edit</th>
+                <th>Delete</th>
+            </tr>
+            <?php foreach ($players as $player) : ?>
+            <tr>
+                <td><?php echo $player['name']; ?></td>
+                <td><?php echo $player['team']; ?></td>
+                <td class="right"><?php echo $player['goals']; ?></td>
+                <td><form action="." method="post">
+                    <input type="hidden" name="action"
+                           value="show_edit_form">
+                    <input type="hidden" name="player_id"
+                           value="<?php echo $player['playerID']; ?>">
+                    <input type="hidden" name="league_id"
+                           value="<?php echo $player['leagueID']; ?>">
+                    <input type="submit" value="Edit">
+                </form></td>
+                <td><form action="." method="post">
+                    <input type="hidden" name="action"
+                           value="delete_player">
+                    <input type="hidden" name="player_id"
+                           value="<?php echo $player['playerID']; ?>">
+                    <input type="hidden" name="league_id"
+                           value="<?php echo $player['leagueID']; ?>">
+                    <input type="submit" value="Delete">
+                </form></td>
+            </tr>
+            <?php endforeach; ?>
+        </table>
+        <p><a href="?action=show_add_form">Add a Player</a></p>
+                </div>
+            </div>
+        </div>
+   <!-- Bootstrap core JavaScript -->
+        <script src="../vendor/jquery/jquery.min.js"></script>
+        <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+        <!-- Menu Toggle Script -->
+        <script>
+            $("#menu-toggle").click(function (e) {
+                e.preventDefault();
+                $("#wrapper").toggleClass("toggled");
+            });
+        </script>
+
+    </body>
+    </html>
